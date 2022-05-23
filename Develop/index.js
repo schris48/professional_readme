0e921adacbf15beeaf0dea31613183a9992b7add
Schris <tovechris48@gmail.com>
2022-05-23 18:30:14 -0400

    add-license-populate-packages-and-questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,94 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const { writeFile, copyFile } = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+          if (titleInput) {
+            return true;
+          } else {
+            console.log('Please enter a title!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter a description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'instructions',
+        message: 'Enter installation instructions (Required)',
+        validate: instructionsInput => {
+          if (instructionsInput) {
+            return true;
+          } else {
+            console.log('Please enter installation instructions!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter project use cases (Required)',
+        validate: usageInput => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log('Please enter the use cases for your project!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose your project license (Required)',
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Mozilla Public License 2.0', 'The Unlicense'],
+        validate: licenseInput => {
+          if (licenseInput) {
+            return true;
+          } else {
+            console.log('Please choose a project license!');
+            return false;
+          }
+        }
+      }
+    ]);
+  };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+promptUser()
+  .then(fileName => {
+    return writeFile(fileName, data);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 // TODO: Create a function to initialize app
 function init() {}
